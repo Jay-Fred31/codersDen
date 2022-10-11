6fb15d7d06fdaf4120526fdc7037dae109fad85a
Jay-Fred31 <70653973+Jay-Fred31@users.noreply.github.com>
2022-10-11 12:34:25 +0100

    week1 and week2 assignment completed

---
+++ week_1/datatype.js
@@ -0,0 +1,17 @@
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let firstName = "Asher"
+console.log(typeof firstName);
+
+let isHappy = true;
+console.log(typeof isHappy);
+
+
+let luxuryCars;
+console.log(typeof luxuryCars);
+
+
+let estates = [];
+console.log(typeof estates);
+
+
---
+++ week_1/index.js
@@ -0,0 +1,48 @@
+//1. comments can make code readable
+
+//2. Welcome to 30DaysOfJavaScript
+
+/*
+3. comments can make code readable, easy to reuse and informative
+*/
+
+// 6. Declare four variables without assigning values
+let houses;
+let profit;
+let moneyCredited;
+let numberOfCodes;
+
+// 7. Declare four variables with assigned values
+
+let name = "Fred";
+let balance = 50;
+let bestRock = "Diamond";
+let isBeautiful = true;
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = 'Joyce';
+let lastName = "Fred";
+let maritalStatus = false;
+let country = 'Spain';
+let age = 16;
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let hisFirstName = 'Joseph', hisLastName = 'Ebuka', myMaritalStatus = true, hisCountry = 'Canada', hisAge = 34;
+
+
+/* 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+I am 25 years old.
+You are 30 years old.
+*/
+
+let myAge = 25;
+let yourAge = 30;
+
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old`)
+
+
+
