0ecb7a21256bf43a9f659bef10b6ccf56f1d5477
Jay-Fred31 <70653973+Jay-Fred31@users.noreply.github.com>
2022-10-31 19:02:20 +0100

    done

--- week_6/index.js
+++ week_6/index.js
@@ -382,5 +382,276 @@ for(let i = 0; i<fullStack.length; i++){
 }
 
 
+// Level 3
 
+// Copy countries array(Avoid mutation)
+
+let immutableCountries = [].concat(countries);
+console.log(immutableCountries);
+
+
+// Arrays are mutable. Create a copy of array which does not modify the original. Sort the copied array and store in a variable sortedCountries.
+let sortedCountries =  immutableCountries.sort();
+console.log(sortedCountries)
+
+// Sort the webTechs array and mernStack array
+let sortedWebTechs = webTechs.sort();
+let sortedMernStack = mernStack.sort();
+
+console.log(sortedWebTechs);
+console.log(sortedMernStack);
+// Extract all the countries contain the word 'land' from the countries array and print it as array
+let countries2  = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Cape Verde',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombi',
+  'Comoros',
+  'Congo (Brazzaville)',
+  'Congo',
+  'Costa Rica',
+  "Cote d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor Timur)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia, The',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia and Montenegro',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe'
+]
+
+let withoutLand = []
+for(country of countries2){
+  if(country.includes('land') == false){
+    withoutLand.push(country);
+  }
+}
+console.log(withoutLand)
+
+// Find the country containing the hightest number of characters in the countries array
+let hightestCharacterCountries = [];
+let highestNum;
+for(let i = 0; i<countries2.length; i++){
+  if(i === 0){
+    highestNum = countries2[0].length;
+  }if(i > 0 && countries2[i].length > highestNum){
+    highestNum = countries2[i].length
+    console.log(highestNum)
+   
+    hightestCharacterCountries.push(countries2[i])
+  }
+}
+console.log(hightestCharacterCountries)
+
+// Extract all the countries contain the word 'land' from the countries array and print it as array
+let withoutLand2 = [];
+for (country of countries2) {
+  if (country.includes("land") == true) {
+    withoutLand2.push(country);
+  }
+}
+console.log(withoutLand2);
+
+// Extract all the countries containing only four characters from the countries array and print it as array
+let fourCharCountries = [];
+for(country of countries2){
+  if(country.length == 4){
+    fourCharCountries.push(country);
+  }
+}
+console.log(fourCharCountries);
+
+// Extract all the countries containing two or more words from the countries array and print it as array
+let twoOrMoreCharCountries = [];
+for (country of countries2) {
+  if (country.length >= 2) {
+    twoOrMoreCharCountries.push(country);
+  }
+}
+console.log(twoOrMoreCharCountries);
+
+// Reverse the countries array and capitalize each country and stored it as an array
+
+let reversedChapsCountries = [];
+for (let i = countries2.length - 1; i >=0; i--) {
+  reversedChapsCountries.push(countries2[i].toUpperCase()) 
+}
+console.log(reversedChapsCountries);
 
